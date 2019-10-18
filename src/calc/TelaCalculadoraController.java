3a8fe9c2ab9055efff4465801eb5dc34d21a84b8
felipenario <felipenario1@gmail.com>
2019-10-17 23:59:30 -0300

    Correções na calculadora. (Falta o botão CE)

--- src/calc/TelaCalculadoraController.java
+++ src/calc/TelaCalculadoraController.java
@@ -3,6 +3,7 @@ package calc;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class TelaCalculadoraController {
@@ -12,61 +13,157 @@ public class TelaCalculadoraController {
     private String sinal = "";
     private double r = 0;
     private double vT = 0;
-    
+    private double vAux = 0;
+    private boolean limpaTela=false;
+
     @FXML
     private TextField tfvisor;
-    
+
+    @FXML
+    private Label campoResult;
+
     @FXML
     void clearClick(ActionEvent event) {
-       v1 = 0;
-       v2 = 0;
-       sinal = "";
-       r = 0;
-       tfvisor.setText("");
+        v1 = 0;
+        vAux = 0;
+        v2 = 0;
+        vT = 0;
+        sinal = "";
+        r = 0;
+        tfvisor.setText("");
+        campoResult.setText("");
     }
 
     @FXML
     void cleareClick(ActionEvent event) {
-       v2 = 0;
-       tfvisor.setText("");
+        v2 = 0;
+        tfvisor.setText("");
+        campoResult.setText(String.valueOf(v1) + String.valueOf(sinal));
+        
     }
-    
+
     @FXML
     void maismenosClick(ActionEvent event) {
-      vT = Double.parseDouble(tfvisor.getText());
-      vT = vT * -1;
-      tfvisor.setText(String.valueOf(vT));
-      vT = 0;
+        vT = Double.parseDouble(tfvisor.getText());
+        vT = vT * -1;
+        tfvisor.setText(String.valueOf(vT));
+        vT = 0;
     }
 
     @FXML
     void numberclick(ActionEvent event) {
-        String num = ((Button)event.getSource()).getText();
+        if (limpaTela){
+            tfvisor.setText("");
+        }
+        String num = ((Button) event.getSource()).getText();
         tfvisor.setText(tfvisor.getText().concat(num));
-       
+        campoResult.setText(campoResult.getText().concat(num));
+        limpaTela=false;
     }
-    
-     @FXML
+
+    /* @FXML
     void operacaoClick(ActionEvent event) {
          
-        v1 = Double.parseDouble(tfvisor.getText());
-        tfvisor.setText("");
-        sinal = ((Button)event.getSource()).getText();
+      
                  
     }
-    
-     @FXML
+     */
+    @FXML
+    void divisaoClick(ActionEvent event) {
+        
+        
+        String num = ((Button) event.getSource()).getText();
+           
+        if (v1 == 0) {
+            
+            v1 = Integer.parseInt(tfvisor.getText());
+            campoResult.setText(campoResult.getText().concat(num));
+           
+        } else {
+            
+            campoResult.setText(campoResult.getText().concat(num));
+            resultadoClick(null);
+            
+        }
+         sinal = "/";
+        limpaTela = true;
+
+    }
+
+    @FXML
+    void maisClick(ActionEvent event) {
+            String num = ((Button) event.getSource()).getText();
+            
+        if (v1 == 0) {
+            
+            v1 = Integer.parseInt(tfvisor.getText());
+            campoResult.setText(campoResult.getText().concat(num));
+          
+        } else {
+            
+            campoResult.setText(campoResult.getText().concat(num));
+            resultadoClick(null);
+            
+        }
+          sinal = "+";
+        limpaTela = true;
+
+    }
+
+    @FXML
+    void menosClick(ActionEvent event) {
+        String num = ((Button) event.getSource()).getText();
+           
+        if (v1 == 0) {
+            
+            v1 = Integer.parseInt(tfvisor.getText());
+            campoResult.setText(campoResult.getText().concat(num));
+            
+        } else {
+            
+            campoResult.setText(campoResult.getText().concat(num));
+            resultadoClick(null);
+            
+        }
+        sinal = "-";
+        limpaTela = true;
+
+    }
+
+    @FXML
+    void multiClick(ActionEvent event) {
+
+        
+        String num = ((Button) event.getSource()).getText();
+           
+        if (v1 == 0) {
+            
+            v1 = Integer.parseInt(tfvisor.getText());
+            campoResult.setText(campoResult.getText().concat(num));
+           
+        } else {
+            
+            campoResult.setText(campoResult.getText().concat(num));
+            resultadoClick(null);
+            
+        }
+         sinal = "*";
+        limpaTela = true;
+        
+        
+    }
+
+    @FXML
     void porcentagemClick(ActionEvent event) {
         v1 = Double.parseDouble(tfvisor.getText());
-        r = v1/100;
+        r = v1 / 100;
         tfvisor.setText(String.valueOf(r));
         v1 = 0;
         r = 0;
-        
-        
+
     }
-    
-      @FXML
+
+    @FXML
     void quadradoClick(ActionEvent event) {
         v1 = Double.parseDouble(tfvisor.getText());
         r = Math.pow(v1, 2);
@@ -74,7 +171,6 @@ public class TelaCalculadoraController {
         v1 = 0;
         r = 0;
     }
-    
 
     @FXML
     void raizClick(ActionEvent event) {
@@ -84,50 +180,116 @@ public class TelaCalculadoraController {
         v1 = 0;
         r = 0;
     }
-    
-     @FXML
+
+    @FXML
     void resultadoClick(ActionEvent event) {
 
-        v2 = Double.parseDouble(tfvisor.getText());
-        
+//        v2 = Double.parseDouble(tfvisor.getText());
+//        tfvisor.setText(String.valueOf(v1) + "+" + String.valueOf(v2));
+        if (event == null) {
+            switch (sinal) {
+                case "+":
+                    v2 = Double.parseDouble(tfvisor.getText());
+                    vT = v1 + v2;
+                    v1 = vT;
+                    tfvisor.setText(String.valueOf(vT));
+                    break;
+
+                case "-":
+                    
+                    v2 = Double.parseDouble(tfvisor.getText());
+                    
+                    vT = v1 - v2;
+                    v1 = vT;
+                    tfvisor.setText(String.valueOf(vT));
+                    break;
+
+                case "*":
+                    v2 = Double.parseDouble(tfvisor.getText());
+                    vT = v1 * v2;
+                    v1 = vT;
+                    tfvisor.setText(String.valueOf(vT));
+                    break;
+
+                case "/":
+                    v2 = Double.parseDouble(tfvisor.getText());
+                    vT = v1 / v2;
+                    v1 = vT;
+                    tfvisor.setText(String.valueOf(vT));
+                    break;
+            }
+        }else{
+
         switch(sinal){
             case "+":
-                r = v1 + v2;
+                v2 = Double.parseDouble(tfvisor.getText());
+                r = vT + v2;
                 tfvisor.setText(String.valueOf(r));
+                campoResult.setText("");
+                v1 = 0;
+                v2 = 0;
+                vT = 0;
+                sinal = "";
                 break;
                 
            case "-":
-                r = v1 - v2;
-                tfvisor.setText(String.valueOf(r));
-                break;
+                v2 = Double.parseDouble(tfvisor.getText());
+                    if(vT < 0){
+                    
+                    r = vT + v2;
+                   v1 = 0;
+                     v2 = 0;
+                       vT = 0;
+                    sinal = "";
+                     tfvisor.setText(String.valueOf(r));
+                }else{
+                    r = vT - v2;
+                    v1 = 0;
+                     v2 = 0;
+                       vT = 0;
+                    sinal = "";
+                    tfvisor.setText(String.valueOf(r));
+        }
+                    break;
                 
            case "*":
-                r = v1 * v2;
+                v2 = Double.parseDouble(tfvisor.getText());
+                r = vT * v2;
                 tfvisor.setText(String.valueOf(r));
+                campoResult.setText("");
+                v1 = 0;
+                v2 = 0;
+                vT = 0;
+                sinal = "";
                 break;
                 
            case "/":
-                r = v1 / v2;
+                v2 = Double.parseDouble(tfvisor.getText());
+                r = vT / v2;
                 tfvisor.setText(String.valueOf(r));
+                campoResult.setText("");
+                v1 = 0;
+                v2 = 0;
+                vT = 0;
+                sinal = "";
                 break;     
         }
-        
-        sinal = "";
-        v1 = 0;
-        v2 = 0;
     }
+        
+    }
+
     @FXML
     void umsobreClick(ActionEvent event) {
         v1 = Double.parseDouble(tfvisor.getText());
-        r = 1/v1;
+        r = 1 / v1;
         tfvisor.setText(String.valueOf(r));
         v1 = 0;
         r = 0;
     }
-    
-     @FXML
+
+    @FXML
     void virgulaClick(ActionEvent event) {
-        String virgula = ((Button)event.getSource()).getText();
+        String virgula = ((Button) event.getSource()).getText();
         tfvisor.setText(tfvisor.getText().concat("."));
     }
 
